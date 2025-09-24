9317c6dae281d53a9eba31dff3b815e292858d21
Betelhem <betelhemyemanezoom@gmail.com>
2025-09-24 15:17:00 +0300

    update api post url

--- components/LeadForm.js
+++ components/LeadForm.js
@@ -11,7 +11,7 @@ export default function LeadForm({ onAdd }) {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/leads', form);
+      const res = await axios.post('https://lead-app-backend-tkmu.onrender.com/leads', form);
       onAdd(res.data);
       setForm({ name: '', email: '', status: 'New' });
     } catch (err) {
